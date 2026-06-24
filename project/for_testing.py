5144e5c23483ae02e8fd9a47e082947371e9958c
so02s <102208102+so02s@users.noreply.github.com>
2026-06-24 13:33:27 +0300

    Create for_testing.py

---
+++ project/for_testing.py
@@ -0,0 +1,35 @@
+from main import robot
+import time
+
+# бесконечный цикл с выводом состояния кнопки
+# while True:
+#     print(robot.get_start_button())
+#     time.sleep(1)
+
+# использование светодиодов
+# for i in range(3, 9):
+#     robot.led[i] = (100, 0, 255)
+# for i in range(3):
+#     robot.led[i] = (100, 0, 255)
+# robot.led.write()
+
+# поменять все светодиоды
+# robot.color(255, 0, 0)
+
+# пищалка
+# robot.beep(1)
+
+# скорость моторов
+# robot.set_motors(100, 100)
+
+# датчик расстояния
+# for i in range(1000):
+    # print(robot.tof.read_range_single_millimeters())
+    # time.sleep(1)
+
+# датчик линии 
+# for i in range(1000):
+#     for i in range(4):
+#         print(robot.get_line(i), end='')
+#     print()
+#     time.sleep(1)
